381c83027f561620a9caa10696e594ec504e9693
Alexandre <alexandre@yacows.com.br>
2021-10-04 14:36:28 -0300

    convertendo msg para pt_Br

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import VueRouter from 'vue-router'
 import { routes } from './routes';
 import './directives/Transform';
 import VeeValidate from 'vee-validate';
+import msg from './pt_BR';
 
 Vue.use(VueResource);
 Vue.http.options.root = 'http://localhost:3000';
@@ -15,7 +16,15 @@ const router = new VueRouter({
   mode: 'history'
 });
 
-Vue.use(VeeValidate);
+Vue.use(VeeValidate, {
+
+  locale: 'pt_Br',
+  dictionary: {
+    pt_Br: {
+      messages: msg      
+    }
+  }
+});
 
 new Vue({
   el: '#app',
